9ba08117d8a306ad1d263ca2659fb0afc4958c39
Lekret <lekret399@mail.ru>
2022-10-09 16:28:34 +0300

    Removed redundant

--- Assets/Scripts/Nodes/Graph/INode.cs
+++ Assets/Scripts/Nodes/Graph/INode.cs
@@ -12,20 +12,4 @@ namespace Nodes.Graph
     {
         Vector2 Position { get; }
     }
-
-    public interface IEdge<TNode, TEdge> 
-        where TNode : IWeightedNode<TNode, TEdge> 
-        where TEdge : IEdge<TNode, TEdge>
-    {
-        int Weight { get; }
-        TNode From { get; }
-        TNode To { get; }
-    }
-
-    public interface IWeightedNode<TNode, TEdge> 
-        where TNode : IWeightedNode<TNode, TEdge> 
-        where TEdge : IEdge<TNode, TEdge>
-    {
-        IEnumerable<TEdge> Edges { get; }
-    }
 }
